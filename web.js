15ef6e62a9673c86e58ce812f243969d3d626196
Rachit Nandwani <rachit504@gmail.com>
2013-07-23 07:19:20 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -4,9 +4,9 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = fs.readFile('index.html');
-  var string = buf.toString();
-  response.send(string);
+  var buf = fs.readFile('index.html','utf8');
+  //var string = buf.toString();
+  response.send(buf);
 });
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
