3a01b70e5d8e5874c8d4e1e991e289dad948b261
Rachit Nandwani <rachit504@gmail.com>
2013-07-23 07:04:30 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
   var buf = fs.readFile('index.html');
   var string = buf.toString();
   response.send(string);
-
+});
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
