5a2321cb196d3c371e7937e5fc8e4f4f4442e192
Rachit Nandwani <rachit504@gmail.com>
2013-08-02 14:46:06 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = fs.readFile('index.html','utf8');
+  var buf = fs.readFileSync('index.html','utf8');
   var string = buf.toString('utf-8');
   response.send(string);
 });
